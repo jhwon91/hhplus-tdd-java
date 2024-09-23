da291d7d88eafdceda28290d36068cf10544e798
won <jwon3991.9191@gmail.com>
2024-09-24 00:37:14 +0900

    PointController 구현 및 로그 추가
    - GET /point/{id} : 특정 유저의 포인트 조회 기능 추가
    - GET /point/{id}/histories : 포인트 충전/이용 내역 조회 기능 추가
    - PATCH /point/{id}/charge : 포인트 충전 기능 추가
    - PATCH /point/{id}/use : 포인트 사용 기능 추가
    - 각 요청에 대해 log.info()로 로그 기록 추가

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -11,6 +11,11 @@ import java.util.List;
 public class PointController {
 
     private static final Logger log = LoggerFactory.getLogger(PointController.class);
+    private final PointService pointService;
+
+    public PointController(PointService pointService) {
+        this.pointService = pointService;
+    }
 
     /**
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
@@ -19,7 +24,8 @@ public class PointController {
     public UserPoint point(
             @PathVariable long id
     ) {
-        return new UserPoint(0, 0, 0);
+        log.info("USER userId: {}", id);
+        return pointService.getUserPoint(id);
     }
 
     /**
@@ -29,7 +35,8 @@ public class PointController {
     public List<PointHistory> history(
             @PathVariable long id
     ) {
-        return List.of();
+        log.info("HISTORY userId: {}", id);
+        return pointService.getUserPointHistory(id);
     }
 
     /**
@@ -40,7 +47,8 @@ public class PointController {
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        log.info("CHARGE userId: {}, amount: {}", id, amount);
+        return pointService.chargeUserPoint(id, amount);
     }
 
     /**
@@ -51,6 +59,7 @@ public class PointController {
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        log.info("USE userId: {}, amount: {}", id, amount);
+        return pointService.useUserPoint(id, amount);
     }
 }
