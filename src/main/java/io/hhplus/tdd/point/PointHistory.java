91629f6e91224c122199023a1e7841caeb40eee2
won <jwon3991.9191@gmail.com>
2024-09-25 00:31:07 +0900

    record PointHistory 생성자 삭제

--- src/main/java/io/hhplus/tdd/point/PointHistory.java
+++ src/main/java/io/hhplus/tdd/point/PointHistory.java
@@ -1,24 +1,10 @@
 package io.hhplus.tdd.point;
 
 public record PointHistory(
-        Long id,
+        long id,
         long userId,
         long amount,
         TransactionType type,
         long updateMillis
 ) {
-
-    // id가 있는 경우의 생성자
-    public PointHistory(Long id, long userId, long amount, TransactionType type, long updateMillis) {
-        this.id = id;
-        this.userId = userId;
-        this.amount = amount;
-        this.type = type;
-        this.updateMillis = updateMillis;
-    }
-
-    // id 없이 객체를 생성하는 생성자 (id는 나중에 자동 생성)
-    public PointHistory(long userId, long amount, TransactionType type, long updateMillis) {
-        this(null, userId, amount, type, updateMillis);  // id는 null로 설정
-    }
 }
