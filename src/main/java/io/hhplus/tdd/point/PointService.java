870a039a2620b150f59eac3b5c6e0a2d4c25ffc9
won <jwon3991.9191@gmail.com>
2024-09-25 23:20:54 +0900

    PointService 주석정리

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -5,7 +5,6 @@ import io.hhplus.tdd.point.repository.UserPointRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class PointService {
@@ -17,10 +16,6 @@ public class PointService {
         this.pointHistoryRepository = pointHistoryRepository;
     }
     /*
-    *  포인트를 충전한다.(O)
-    *  포인트를 사용한다.(O)
-    *  포인트를 조회한다.(O)
-    *  포인트 내역을 조회한다.(O)
     *  잔고가 부족할 경우, 포인트 사용은 실패하여야 합니다. (X)
     *  동시에 여러 건의 포인트 충전, 이용 요청이 들어올 경우 순차적으로 처리되어야 합니다. (X)
     */
