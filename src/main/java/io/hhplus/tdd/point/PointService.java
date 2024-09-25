b89f8582a3e13ca442f0ad859763f00d1e8fb773
won <jwon3991.9191@gmail.com>
2024-09-26 00:38:02 +0900

    STEP_1 포인트 충전, 사용에 대한 정책 추가 PointService 검증 추가 - 최대 잔고 초과 충전 검증 추가 - 0 보다 같거나 작은 충전 검증 추가 - 0 보다 같거나 작은 사용 검증 추가 PointServiceTest 테스트 추가 - 포인트_충전_초과_실패 테스트 추가 - 음수또는_0값으로_포인트_충전시_실패 - 음수또는_0값으로포인트_사용시_실패

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 @Service
 public class PointService {
+    private static final long MAX_POINT_BALANCE = 1000L; // 최대 잔고 제한 설정
+
     private final UserPointRepository userPointRepository;
     private final PointHistoryRepository pointHistoryRepository;
 
@@ -16,13 +18,21 @@ public class PointService {
         this.pointHistoryRepository = pointHistoryRepository;
     }
     /*
-    *  잔고가 부족할 경우, 포인트 사용은 실패하여야 합니다. (X)
     *  동시에 여러 건의 포인트 충전, 이용 요청이 들어올 경우 순차적으로 처리되어야 합니다. (X)
     */
 
     // 특정 사용자의 포인트 충전 로직 (포인트를 충전한다.)
     public UserPoint chargeUserPoint(long userId, long amount) {
         UserPoint userPoint = userPointRepository.findById(userId);
+
+        if (amount <= 0) {
+            throw new IllegalArgumentException("포인트 충전은 0 보다 커야 합니다.");
+        }
+
+        if (userPoint.point() + amount > MAX_POINT_BALANCE) {
+            throw new IllegalArgumentException("최대 잔고를 초과 했습니다.");
+        }
+
         UserPoint updateUserPoint = new UserPoint(userPoint.id(), userPoint.point() + amount, System.currentTimeMillis());
         userPointRepository.save(updateUserPoint);
         pointHistoryRepository.save(new PointHistory(0,userId, amount, TransactionType.CHARGE, System.currentTimeMillis()));
@@ -31,8 +41,13 @@ public class PointService {
     }
 
     // 특정 사용자의 포인트 사용 로직 (포인트를 사용한다.)
+    // 잔고가 부족할 경우, 포인트 사용은 실패하여야 합니다.
     public UserPoint useUserPoint(long userId, long amount) {
         UserPoint userPoint = userPointRepository.findById(userId);
+
+        if (amount <= 0) {
+            throw new IllegalArgumentException("포인트 사용은 0 보다 커야 합니다.");
+        }
 
         if (userPoint.point() < amount) {
             throw new IllegalArgumentException("잔액이 부족합니다.");
